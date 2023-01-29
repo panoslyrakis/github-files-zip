b12b741474cef14d1029d7642abce9ba5ec12729
Panos Lyrakis <panos.lyrakis@gmail.com>
2023-01-29 20:45:18 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Github File Download`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}

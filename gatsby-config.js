f85d6411832abe57be00086fe57faa5f798f459c
Panos Lyrakis <panos.lyrakis@gmail.com>
2023-01-29 21:57:15 +0200

    First build gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "github-files-zip",
   siteMetadata: {
     title: `Github File Download`,
     siteUrl: `https://www.yourdomain.tld`,
